46aa129ecf561c1590844696e26bf43321627d78
Alexandre-Alberti <125623015+Alexandre-Alberti@users.noreply.github.com>
2024-06-03 19:35:44 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import webbrowser
 
 # Interface do Streamlit
 st.title("Aplicação de Soma")
@@ -8,9 +9,17 @@ a = st.number_input("Digite o valor de A", value=0)
 b = st.number_input("Digite o valor de B", value=0)
 
 # Botão para calcular a soma
-if st.button("Calcular", key="calcular_button", help="Clique para calcular a soma"):
+if st.button("Calcular", help="Clique para calcular a soma"):
     # Cálculo da soma
     soma = a + b
     resultado_texto = f"A soma de {a} + {b} é igual a: {soma}"
-    # Exibir o resultado em uma nova janela usando JavaScript
-    st.write(f'<script>window.open("data:text/html,<h2>{resultado_texto}</h2>","_blank");</script>', unsafe_allow_html=True)
+    # Exibir o resultado
+    st.write(resultado_texto)
+    # Abrir o resultado em uma nova janela do navegador
+    script = f"""
+    <script>
+    var myWindow = window.open("", "_blank", "width=600,height=400");
+    myWindow.document.write("<p>{resultado_texto}</p>");
+    </script>
+    """
+    st.write(script, unsafe_allow_html=True)
